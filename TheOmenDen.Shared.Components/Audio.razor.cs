b7b9bd9b388d29abb7204cad2974487ebc113fe1
Andrew Beers <andrew.d.beers@gmail.com>
2022-08-16 15:04:52 -0400

    Add project files.

---
+++ TheOmenDen.Shared.Components/Audio.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TheOmenDen.Shared.Components;
+
+public partial class Audio : ComponentBase
+{
+    [Parameter] public Boolean Muted { get; set; } = true;
+    [Parameter] public Boolean Autoplay { get; set; } = true;
+
+    [Parameter] public IEnumerable<Uri> Sources { get; set; } = Enumerable.Empty<Uri>();
+
+    [Parameter] public String CodeNotSupportedMessage { get; set; } = "Your browser does not support the audio element.";
+
+
+
+}
