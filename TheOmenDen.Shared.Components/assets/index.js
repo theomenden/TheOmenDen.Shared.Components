478f09e8d2e98c3707b7e4e0945ffe954791b57c
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-07-27 16:25:59 -0400

    Clean up the project architecture

--- TheOmenDen.Shared.Components/assets/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dynamicScriptLoader_1 = require("./dynamicScriptLoader");
-const reCAPTCHAInterop_1 = require("./reCAPTCHAInterop");
-window.scriptLoader = new dynamicScriptLoader_1.ScriptLoader();
-window.reCaptchaInterop = new reCAPTCHAInterop_1.CaptchaLoader();
-//# sourceMappingURL=index.js.map
